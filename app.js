a0c8120a2a654d9eb5898332d4db25b8b4c01198
abhishekv <abhishekv@orowealth.com>
2021-09-20 20:46:25 +0530

    removed boilerplate files

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/auth', auth);
 app.use('/stores', stores);
 
